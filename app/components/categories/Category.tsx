c33cbd1794a701ebf4967c236e4732b0fa5df5bb
Nikolay Nikiforuk <nnp.nikiforuk@gmail.com>
2025-11-04 00:21:09 +0200

    added categiries

---
+++ app/components/categories/Category.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import Image from 'next/image';
+
+import ButtonArrow from '../buttons/ButtonArrow';
+
+import arrowLeftIcon from '../../assets/icons/arrow-left.svg';
+import arrowRightIcon from '../../assets/icons/arrow-right.svg';
+import categoryPhonesIcon from '../../assets/icons/category-phones.svg';
+import categoryWatchesIcon from '../../assets/icons/category-watches.svg';
+import categoryHpIcon from '../../assets/icons/category-hp.svg';
+import categoryCamerasIcon from '../../assets/icons/category-cameras.svg';
+import categoryComputersIcon from '../../assets/icons/category-computers.svg';
+import categoryGamingIcon from '../../assets/icons/category-gaming.svg';
+
+import styles from '../../ui/categories/Category.module.scss';
+
+export default function Category() {
+  const handleClickLeft = () => {
+    console.log('left');
+  }
+
+  const handleClickRight = () => {
+    console.log('left');
+  }
+  return (
+    <section className={styles.container}>
+      <div className={styles.top}>
+        <h2 className={styles.top_title}>
+          Browse By Category
+        </h2>
+        <div className={styles.top_groupButtons}>
+          <ButtonArrow callback={handleClickLeft} icon={arrowLeftIcon} />
+          <ButtonArrow callback={handleClickRight} icon={arrowRightIcon} />
+        </div>
+      </div>
+      <div className={styles.categories}>
+        <div className={styles.category}>
+          <Image width={48} height={48} src={categoryPhonesIcon} alt='phones-icon' />
+          <p className={styles.category_text}>Phones</p>
+        </div>
+        <div className={styles.category}>
+          <Image width={48} height={48} src={categoryWatchesIcon} alt='phones-icon' />
+          <p className={styles.category_text}>Smart Watches</p>
+        </div>
+        <div className={styles.category}>
+          <Image width={48} height={48} src={categoryHpIcon} alt='phones-icon' />
+          <p className={styles.category_text}>Cameras</p>
+        </div>
+        <div className={styles.category}>
+          <Image width={48} height={48} src={categoryCamerasIcon} alt='phones-icon' />
+          <p className={styles.category_text}>Computers</p>
+        </div>
+        <div className={styles.category}>
+          <Image width={48} height={48} src={categoryComputersIcon} alt='phones-icon' />
+          <p className={styles.category_text}>Phones</p>
+        </div>
+        <div className={styles.category}>
+          <Image width={48} height={48} src={categoryGamingIcon} alt='phones-icon' />
+          <p className={styles.category_text}>Gaming</p>
+        </div>
+      </div>
+    </section>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "./components/banners/Banner";
 import SmallerBanners from "./components/banners/SmallerBanners";
+import Category from "./components/categories/Category";
 
 export default function Home() {
   return (
     <main className="root-container">
       <Banner />
       <SmallerBanners />
+      <Category />
     </main>
   );
 }
